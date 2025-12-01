43b7f448a39580ab0e3a4ca68453c7fd59961ea3
Wisit <Wisit.thampreecha@e-tech.ac.th>
2025-12-01 09:57:39 +0700

    "second Commit"

--- lib/main.dart
+++ lib/main.dart
@@ -35,13 +35,13 @@ class MyHomePage extends StatelessWidget {
           children: [
             Container(
               width: double.infinity,
-              decoration: BoxDecoration(color: Colors.red[900]),
+              decoration: BoxDecoration(color: Colors.black),
               child: Column(
                 children: [
                   Text(
                     "ข้อมูลส่วนตัว",
                     style: TextStyle(
-                      fontSize: 22,
+                      fontSize: 24,
                       fontWeight: FontWeight.w600,
                       color: Colors.white,
                     ),
@@ -63,9 +63,16 @@ class MyHomePage extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 10),
-                  Text("Wisit Thampreecha"),
+                  Text(
+                    "Wisit Thampreecha",
+                    style: TextStyle(color: Colors.white),
+                  ),
                   SizedBox(height: 10),
-                  Text("wisit.thampreecha@e-tech.ac.th"),
+                  Text(
+                    "wisit.thampreecha@e-tech.ac.th",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 18),
                 ],
               ),
             ),
@@ -76,75 +83,87 @@ class MyHomePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text("ข้อมูลส่วนตัว"),
+                  SizedBox(height: 18),
                   Row(
                     children: [
                       Container(
                         padding: EdgeInsets.all(10),
                         decoration: BoxDecoration(
                           color: Colors.green,
-                          borderRadius: BorderRadius.circular(5),
+                          borderRadius: BorderRadius.circular(15),
                         ),
-                        child: Icon(Icons.phone),
+                        child: Icon(Icons.phone, color: Colors.greenAccent),
                       ),
                       SizedBox(width: 18),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [Text("เบอร์โทร"), Text("064-334-0472")],
                       ),
                     ],
                   ),
+                  SizedBox(height: 18),
                   Row(
                     children: [
                       Container(
                         padding: EdgeInsets.all(10),
                         decoration: BoxDecoration(
                           color: Colors.pink,
-                          borderRadius: BorderRadius.circular(5),
+                          borderRadius: BorderRadius.circular(15),
                         ),
-                        child: Icon(Icons.cake),
+                        child: Icon(Icons.cake, color: Colors.purple),
                       ),
                       SizedBox(width: 18),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [Text("วันเกิด"), Text("05/04/2549")],
                       ),
                     ],
                   ),
+                  SizedBox(height: 18),
                   Row(
                     children: [
                       Container(
                         padding: EdgeInsets.all(10),
                         decoration: BoxDecoration(
                           color: Colors.orange,
-                          borderRadius: BorderRadius.circular(5),
+                          borderRadius: BorderRadius.circular(15),
                         ),
-                        child: Icon(Icons.location_pin),
+                        child: Icon(Icons.location_pin, color: Colors.red),
                       ),
                       SizedBox(width: 18),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [Text("ที่อยู่"), Text("ชลบุรี")],
                       ),
                     ],
                   ),
+                  SizedBox(height: 18),
                   Row(
                     children: [
                       Container(
                         padding: EdgeInsets.all(10),
                         decoration: BoxDecoration(
                           color: Colors.purple,
-                          borderRadius: BorderRadius.circular(5),
+                          borderRadius: BorderRadius.circular(15),
                         ),
                         child: Icon(Icons.school),
                       ),
                       SizedBox(width: 18),
                       Column(
-                        children: [Text("การศึกษา"), Text("วิทยาลัยเทคโนโลยีภาคตะวันออก อีเทค")],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("การศึกษา"),
+                          Text("วิทยาลัยเทคโนโลยีภาคตะวันออก อีเทค"),
+                        ],
                       ),
                     ],
                   ),
-
+                  SizedBox(height: 18),
                   ElevatedButton(
                     onPressed: () => Navigator.pushNamed(context, '/second'),
                     child: Text("ไปหน้า 2"),
                     style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.fromLTRB(150, 5, 150, 5),
                       backgroundColor: Colors.orange,
                       foregroundColor: Colors.white,
                     ),
@@ -164,6 +183,146 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Second Page")));
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        child: ClipOval(
+                          child: Image.network(
+                            "https://e1.pngegg.com/pngimages/677/263/png-clipart-picsart-shinnosuke-nohara-video-crayon-shinchan-nene-sakurada-emission-de-television-meme-internet-youtube-blague-instagram.png",
+                            width: 80,
+                            height: 80,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Column(children: [
+                        Text("724.9k", style: TextStyle(color: Colors.black)),
+                        Text("กำลังติดตาม", style: TextStyle(color: Colors
+                            .black)),
+                      ]),
+                      SizedBox(width: 10),
+                      Container(width: 1.5,height: 40,color:  Colors.grey.shade50),
+                      SizedBox(width: 20),
+                      Column(children: [
+                        Text("9.8m", style: TextStyle(color: Colors.black)),
+                        Text("ผู้ติดตาม", style: TextStyle(color: Colors
+                            .black)),
+                      ]),
+                      SizedBox(width: 10),
+                      Container(width: 1.5,height: 40,color:  Colors.grey.shade50),
+                      SizedBox(width: 20),
+                      Column(children: [
+                        Text("23.4m", style: TextStyle(color: Colors.black)),
+                        Text("ถูกใจและบันทึก", style: TextStyle(color: Colors
+                            .black)),
+                      ]),
+                    ],
+                  ),
+                  SizedBox(width: 25, height: 25),
+                  Container(
+                    width: double.infinity,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Wisit Thampreecha",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(Icons.verified, color: Colors.blue),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 1),
+            Container(
+              width: double.infinity,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 5),
+                  Icon(Icons.tiktok, color: Colors.grey, size: 20),
+                  Text(
+                    "wisit_thampreecha",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(width: 2),
+                  Icon(Icons.arrow_drop_down, color: Colors.black),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () => Navigator.pushNamed(context, '/second'),
+                    child: Text("ติดตาม"),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(300, 30),
+                      backgroundColor: Colors.yellow,
+                      foregroundColor: Colors.black,
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        SizedBox(height: 10, width: 10),
+                        Icon(Icons.share, color: Colors.black),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 15),
+              width: double.infinity,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 10),
+                  Image.network(
+                    "https://stickershop.line-scdn.net/stickershop/v1/product/10986/LINEStorePC/main.png",
+                    width: 190,
+                    height: 320,
+                  ),
+                  SizedBox(width: 10),
+                  Image.network(
+                    "https://stickershop.line-scdn.net/stickershop/v1/product/6451/LINEStorePC/main.png?v=1",
+                    width: 163,
+                    height: 320,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
